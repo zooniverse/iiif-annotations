070ee01f0a2dcd867cfdcc8e7a9afd68012b153f
Jim O'Donnell <jim@zooniverse.org>
2022-03-07 15:24:09 +0000

    Filter out PFE classifications

--- .eleventy.js
+++ .eleventy.js
@@ -6,10 +6,12 @@ module.exports = function (eleventyConfig) {
       columns: true,
       skip_empty_lines: true,
     })
-    const classifications = records.map(({ annotations, subject_data }) => ({
+    const classifications = records.map(({ metadata, annotations, subject_data }) => ({
+      metadata: JSON.parse(metadata),
       annotations: JSON.parse(annotations),
       subjectMetadata: JSON.parse(subject_data)
     }))
+    .filter(classification => classification.metadata.classifier_version === '2.0')
     console.log('read', classifications.length, 'classifications')
     return classifications
   });
