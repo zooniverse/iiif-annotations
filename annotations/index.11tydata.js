d566dce7672fdcdf34220351aa44377618b72428
Jim O'Donnell <jim@zooniverse.org>
2022-03-07 15:48:06 +0000

    Output annotation, canvas and subject data

--- annotations/index.11tydata.js
+++ annotations/index.11tydata.js
@@ -1,11 +1,11 @@
 function annotations({ manifest, classifications }) {
-  const annotations = classifications.map(classification => {
+  return classifications.map(classification => {
     const { metadata, annotations, subjectMetadata } = classification
-    const [ annotation ] = annotations
-    
-    return { annotation, metadata, subjectMetadata }
+    const canvasIndex = subjectMetadata['#priority'] - 1
+    const [ sequence ] = manifest.sequences
+    const canvas = sequence.canvases[canvasIndex]
+    return { annotations, canvas, subjectMetadata }
   })
-  return annotations
 }
 
 module.exports = {
