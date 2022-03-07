76b8c7966e26ee62c1ef634f72e85595cffae72c
Jim O'Donnell <jim@zooniverse.org>
2022-03-07 17:01:38 +0000

    Add annotation bodies and targets

--- annotations/index.11tydata.js
+++ annotations/index.11tydata.js
@@ -1,23 +1,32 @@
 const MAX_SUBJECT_HEIGHT = 2000
 const MAX_SUBJECT_WIDTH = 1400
 
-function transformRectangles(annotations, canvas) {
+function scaledRectangle(rectangle, canvas) {
   const scale = Math.max((canvas.height / MAX_SUBJECT_HEIGHT), (canvas.width / MAX_SUBJECT_WIDTH))
+  const { x_center, y_center, width, height } = rectangle
+  const x = parseInt((x_center - (width / 2)) * scale, 10)
+  const y = parseInt((y_center - (height / 2)) * scale, 10)
+  const w = parseInt(width * scale, 10)
+  const h = parseInt(height * scale, 10)
+  return { x, y, w, h }
+}
+function transformRectangles(annotations, canvas) {
   const drawing = annotations.find(annotation => annotation.task === 'T0')
   const rectangles = drawing.value
   return rectangles.map((rectangle, index) => {
-    const { x_center, y_center, width, height } = rectangle
+    const { x, y, w, h } = scaledRectangle(rectangle, canvas)
     const textAnnotation = annotations.find(annotation => annotation.markIndex === index)
     const label = textAnnotation.value
-    const x = x_center - (width / 2)
-    const y = y_center - (height / 2)
     return {
-      canvas: canvas['@id'],
-      x: x * scale,
-      y: y * scale,
-      width: width * scale,
-      height: height * scale,
-      label
+      type: 'Annotation',
+      motivation: 'tagging',
+      body: {
+        type: 'TextualBody',
+        value: label,
+        language: 'en',
+        format: 'text/plain'
+      },
+      target: `${canvas['@id']}#xywh=${x},${y},${w},${h}`
     }
   })
 }
